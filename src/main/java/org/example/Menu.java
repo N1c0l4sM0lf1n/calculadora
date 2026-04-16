ba819b0dbbd1e8bff7c2cc9fa3340c7556f6fecd
N1c0l4sM0lf1n <n.molfinqueo01@ufromail.cl>
2026-04-15 23:20:15 -0400

    Create Menu.java

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,11 @@
+public class Menu {
+
+    public static void mostrar() {
+        System.out.println("1. Operaciones básicas");
+        System.out.println("2. Ecuación cuadrática");
+        System.out.println("3. Geometría");
+        System.out.println("4. Sistema ecuaciones");
+        System.out.println("5. Recta");
+        System.out.println("6. Salir");
+    }
+}
